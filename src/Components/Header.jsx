f0590e5f0dbbecccaf20486fce5d011cebb1009f
a.sulaymonov1177@gmail.com <Abdurahmon1177@gmail.com>
2022-07-13 17:24:27 +0500

    asdfsdaf

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import headerImg from "./Imgs/Nurinvest1.jpg";
+import headerImg from "./Imgs/bg/oq.png";
 
 function Header(props) {
   return (
@@ -10,11 +10,11 @@ function Header(props) {
             <img src={headerImg} alt="" />
           </div>
           <div className="col-lg-6 col-md-10 col-sm-12 header__content-right">
-            <h2>Halol Investitsiya</h2>
-            <p>
+            <h2 className="title_header">Halol Investitsiya</h2>
+            <p className="text_header">
             Pul o'zi uchun ishlashini istaydiganlar uchun kurs!
             </p>
-            <button className="header__content-btn">Button</button>
+            <button className="header__content-btn">Online kurs</button>
           </div>
         </div>
       </div>
